c83ec08ef253bae77f364c6d4fbb99cbaef5d2b1
noel nyakundi <noel.nyakundi@student.moringaschool.com>
2024-03-27 13:40:43 +0300

    filter

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+const drivers = [     'Wilfred',    'Pauline',     'Javier',      'Joshua',  ];
+
+function findMatching(drivers, string) {
+    return drivers.filter(driver =>
+        driver.toUpperCase() === string.toUpperCase()
+    );
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver =>
+        driver.toLowerCase().startsWith(letters.toLowerCase())
+    );
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(drivers =>
+        drivers.name.toLowerCase() === name.toLowerCase()
+    );
+}
+
